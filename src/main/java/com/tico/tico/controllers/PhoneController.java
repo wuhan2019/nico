8382513930be37eab062a31a4bdd39ebee88377f
Gwen Chen <38069331+2433574201@users.noreply.github.com>
2019-04-07 22:13:36 +0800

    2019.4.7

--- src/main/java/com/tico/tico/controllers/PhoneController.java
+++ src/main/java/com/tico/tico/controllers/PhoneController.java
@@ -5,6 +5,7 @@ import com.tico.tico.entities.Phone;
 import com.tico.tico.services.Comment2PhoneService;
 import com.tico.tico.services.PhoneService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,7 +13,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import javax.websocket.server.PathParam;
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -32,6 +36,8 @@ public class PhoneController {
     @GetMapping("/getPhoneByName/{phone_name}")
     public String getPhoneByName(@PathVariable("phone_name") String name,
                                  Model model){
+
+
         Phone phone = phoneService.getPhoneByName(name);
         //添加评论信息
         List<Comment2Phone> phoneComments = comment2PhoneService.getByName(name);
@@ -98,6 +104,31 @@ public class PhoneController {
             model.addAttribute("searchPhone"
                     ,"找不到您要的商品！");
         }
+        return "/success";
+    }
+
+    //把指定的商品加入对比队列
+    @GetMapping("addToCompare/{name}")
+    public String addToCompare(@PathVariable("name") String name,
+                               HttpServletRequest request){
+        List<String> phoneList = (List<String>) request.getSession().getAttribute("phoneList");
+        phoneList.add(name);
+        return "/success";
+    }
+    //进行对比
+    @GetMapping("/phoneCompare")
+    public String phoneCompare(HttpServletRequest request,Model model){
+        List<String> phoneList = (List<String>) request.getSession().getAttribute("phoneList");
+        List<Phone> result = new ArrayList<>();
+        if(phoneList.size()<=1){
+            model.addAttribute("phoneCompare","请选择两个以上的商品");
+        }else{
+            for(String name:phoneList){
+                result.add(phoneService.getPhoneByName(name));
+            }
+            model.addAttribute("phoneCompare",result);
+        }
+
         return "/success";
     }
 }
--- src/main/java/com/tico/tico/controllers/UserController.java
+++ src/main/java/com/tico/tico/controllers/UserController.java
@@ -1,6 +1,6 @@
 package com.tico.tico.controllers;
 
-import com.tico.tico.entities.User;
+import com.tico.tico.entities.*;
 import com.tico.tico.services.UserService;
 import jdk.nashorn.internal.ir.RuntimeNode;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +9,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpSession;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -34,7 +35,18 @@ public class UserController {
                 && userService.getPasswordByName(user_name).equals(user_password)){
             //登录成功处理，会话保存user_name
             session.setAttribute("user_name",user_name);
-           return "redirect:/success";
+            //登录后，我们用一个会话保存九个产品需要对比的产品
+            List<String> phoneList = new ArrayList<>();session.setAttribute("phoneList",phoneList);
+            List<String> ssdList = new ArrayList<>();session.setAttribute("ssdList",ssdList);
+            List<String> panelList = new ArrayList<>();session.setAttribute("panelList",panelList);
+            List<String> mouseList = new ArrayList<>();session.setAttribute("mouseList",mouseList);
+            List<String> memmoryList = new ArrayList<>();session.setAttribute("memmoryList",memmoryList);
+            List<String> laptopList = new ArrayList<>();session.setAttribute("laptopList",laptopList);
+            List<String> keyboardList = new ArrayList<>();session.setAttribute("keyboardList",keyboardList);
+            List<String> headsetList = new ArrayList<>();session.setAttribute("headsetList",headsetList);
+            List<String> gpuList = new ArrayList<>();session.setAttribute("gpuList",gpuList);
+
+            return "redirect:/success";
         }
         //登录失败处理，回到登录页面，显示错误信息
         model.put("msg","用户名密码错误");
